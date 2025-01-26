c8f9c53ad30cfafa5e7ff6a26efd2c3cf72f1e19
ginzburg-dev <ginzburgdd@gmail.com>
2025-01-26 22:21:36 +0000

    Modify break_continue.cpp: Fix comments for clarity

--- loops/break_continue.cpp
+++ loops/break_continue.cpp
@@ -10,26 +10,26 @@ int main()
     {
     case 1:
         std::cout << "You've got an option #1\n";
-        break; // jump out of switch block
+        break; // exit the switch block
     case 2:
         std::cout << "You've got an option #2\n";
-        break; // jump out of switch block
+        break; // exit the switch block
     case 3:
         std::cout << "You've got an option #3\n";
-        break; // jump out of switch block
+        break; // exit the switch block
     default:
         std::cout << "Unknown\n";
-        break; // jump out of switch block
+        break; // exit the switch block
     }
 
 
     for (int i{ 0 }; i <= 100; ++i)
     {
         if ( (i % 7 == 0) || (i % 5 == 0) )
-            continue; // skip the loop pass iteration
+            continue; // skip the current iteration of the loop
 
         std::cout << i << '\n';
-        // jump to this point and incriment the counter
+        // jump to this point and increment the counter variable 'i'
     }
 
     return 0;
