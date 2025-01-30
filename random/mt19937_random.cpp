7c62e29a3eb220e4942dd0fc8d91ad70f4322e28
ginzburg-dev <ginzburgdd@gmail.com>
2025-01-30 17:00:18 +0000

    Add mt19937_random.cpp: Examples of using  Mersenne Twister pseudo random number generation, seeding with different methoids like chrono::steady_clock, random_device, seed_sequence

---
+++ random/mt19937_random.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <random>
+#include <chrono>
+
+int main()
+{
+    std::mt19937 mt{ static_cast<std::mt19937::result_type>( 
+        std::chrono::steady_clock::now().time_since_epoch().count() ) 
+        }; // seed a 32-bit Mersenne Twister usind steady_clock
+
+    std::mt19937 mt_rd{ std::random_device{}() }; // seeding by random_device{}.operator()
+
+    std::random_device rd{};
+    std::seed_seq ss{ rd(), rd(), rd(), rd(),  rd(), rd(), rd(), rd()};
+    std::mt19937 mt_ss{ std::random_device{}() }; // seeding by seed sequence of 8 random_device values
+
+    std::uniform_int_distribution<> die6{1, 6};
+
+    // steady_clock
+    for (int i{ 1 }; i <= 50; ++i)
+    {
+        std::cout << die6(mt) << '\t'; // generate a random number
+        if (i % 5 == 0) // if we've printed 5, start a new row
+            std::cout << '\n';
+    }
+
+    std::cout << '\n';
+
+    // random device
+    for (int i{ 1 }; i <= 50; ++i)
+    {
+        std::cout << die6(mt_rd) << '\t'; // generate a random number
+        if (i % 5 == 0) // if we've printed 5, start a new row
+            std::cout << '\n';
+    }
+
+    std::cout << '\n';
+
+    // seed sequence
+    for (int i{ 1 }; i <= 50; ++i)
+    {
+        std::cout << die6(mt_ss) << '\t'; // generate a random number
+        if (i % 5 == 0) // if we've printed 5, start a new row
+            std::cout << '\n';
+    }
+
+
+    return 0;
+}
