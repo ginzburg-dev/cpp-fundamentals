44f7a93687c25338b6749316f9ba7453f8440ab6
ginzburg-dev <ginzburgdd@gmail.com>
2025-02-02 01:01:07 +0000

    Modify prime_numbers.cpp: Add comments

--- loops/prime_numbers.cpp
+++ loops/prime_numbers.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
+// retrn true value if given number is prime
 bool isPrime(int x)
 {
     if ( x <= 0 )
         return false;
 
+    // loop from 2 to square root of current number std::sqrt(x) 
     for (int i{ 2 }; i * i <= x; ++i)
     {
         if (x % i == 0)
@@ -18,9 +20,9 @@ int main()
 {
     for (int i{1}; i <= 100; ++i)
     {
-        assert(isPrime(i));
         if (isPrime(i))
             std::cout << i << '\n';
     }
+
     return 0;
 }
