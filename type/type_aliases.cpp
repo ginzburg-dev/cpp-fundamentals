f1569dfc5bfa26ec5fc942041c515a7b38170678
ginzburg-dev <ginzburgdd@gmail.com>
2025-02-09 16:27:17 +0000

    Modify type_aliases.cpp

--- type/type_aliases.cpp
+++ type/type_aliases.cpp
@@ -1,4 +1,6 @@
+#include "type_aliases_h.h"
 #include <iostream>
+
 
 using Distance = long double;
 typedef double Height; // prefer not to use it, unless for backwards capability
@@ -12,6 +14,10 @@ int main()
 {
     Distance distance{ getDistance(5, 19) };
     std::cout << distance << '\n';
+
+    // Weight from type_aliases_h.h
+    Weight weight{ 3.2 };
+    std::cout << weight << '\n';
 
     return 0;
 }
