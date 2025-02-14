76d86945b41b4ff497486ad507a1758b2113b832
ginzburg-dev <ginzburgdd@gmail.com>
2025-02-14 00:16:01 +0000

    Add constexpr_part_one.cpp

--- functions/constexpr_part_one.cpp
+++ functions/constexpr_part_one.cpp
@@ -1,7 +1,23 @@
 #include "constexpr_func_multifile.h"
 #include <iostream>
+#include <type_traits>
 
-consteval auto compileTimeOnly(int x) // consteval are always evaluated at compile-time
+int nonConstFunc(int x)
+{
+    return x;
+}
+
+// To make a constexpr function work at both compile-time and runtime 
+// while including a non-const function call, use std::is_constant_evaluated()
+constexpr auto someFunc(int x) 
+{
+    if ( std::is_constant_evaluated() )
+        return x + 10;
+    else
+        return nonConstFunc(x);
+}
+
+consteval auto compileTimeOnly(int x) // C++ 20: consteval are always evaluated at compile-time
 {
     return x * 10;
 }
@@ -28,6 +44,12 @@ int main()
 
     constexpr double multifileConstexprMult{ mult(1.3, 3.3) }; // using constexpr func from header to init
     std::cout << multifileConstexprMult << '\n';
+
+    std::cout << compileTimeOnly(mix<int,int>(10,20))<< '\n'; // in this case constexpr function must be evaluated at compile-time
+
+    constexpr auto compileTimeTest{ someFunc(1) }; // test someFunc() to ensure that it works in compile-time
+    std::cout << compileTimeTest << '\n'; // compile time
+    std::cout << someFunc(1) << '\n'; // runtime
 
     return 0;
 }
