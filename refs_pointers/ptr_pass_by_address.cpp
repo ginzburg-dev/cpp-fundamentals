0aaa94a2c853c8aea56fb82b3947f2e64f79d1c5
ginzburg-dev <ginzburgdd@gmail.com>
2025-02-16 14:01:17 +0000

    Update ptr_pass_by_address.cpp

--- refs_pointers/ptr_pass_by_address.cpp
+++ refs_pointers/ptr_pass_by_address.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 #include <string>
 #include <cassert>
+#include <cstddef> // dor std::nullptr_t
+
+void print(std::nullptr_t)
+{
+    std::cout << "in print(std::nullptr_t)\n";
+}
+void print(int*)
+{
+    std::cout << "in print(int*)\n";
+}
+
+void add(int* ptr2)
+{
+    ++*ptr2;
+}
+
+void nullify(int*& refptr)
+{
+    refptr = nullptr;
+}
 
 void printByReference(const std::string& str)
 {
@@ -25,6 +45,19 @@ int main()
     std::string s { "Hello World!" };
     printByAddress(&s);
     printByReference(s); // Prefer pass by reference to pass by address unless you have a specific reason to use pass by address.
+
+    int x { 1 };
+    add(&x);
+    std::cout << x << '\n';
+
+    int* ptr { &x };
+    std::cout << "ptr is " << (ptr ? "non-null\n" : "null\n");
+    nullify(ptr);
+    std::cout << "ptr is " << (ptr ? "non-null\n" : "null\n");
+
+    print(nullptr); // calls print(std::nullptr_t)
+    ptr = nullptr;
+    print(ptr); // calls print(int*) since ptr has type int*
 
     return 0;
 }
