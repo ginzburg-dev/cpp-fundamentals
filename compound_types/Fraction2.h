41e8250fcc2e5896b3dbcd7b109be131927d14a4
ginzburg-dev <ginzburgdd@gmail.com>
2025-02-17 16:42:14 +0000

    Update Fraction2.h

--- compound_types/Fraction2.h
+++ compound_types/Fraction2.h
@@ -1,6 +1,7 @@
 #ifndef FRACTION2_H
 #define FRACTION2_H
 
+// this type is allowed to be defined in multiple code files. It exempt from the one-definition rule.
 struct Fraction2
 {
     int numerator{};
