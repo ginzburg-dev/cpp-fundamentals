216a7c7149e19852c6aab41b1e1079e6e926ffc8
ginzburg-dev <ginzburgdd@gmail.com>
2025-02-18 21:58:12 +0000

    Modify struct.cpp

--- compound_types/struct.cpp
+++ compound_types/struct.cpp
@@ -7,9 +7,44 @@ struct Employee
     double wage {};
 };
 
+struct Computer
+{
+    int processor;    // 0 by default
+    int ram { 32 };   // 32 by default
+    double price {};  // 0 by default
+};
+
+std::ostream& operator<<(std::ostream& out, const Computer& computer)
+{
+    out << "processor: " << computer.processor << " RAM: " << computer.ram << " price: " << computer.price;
+    return out;
+}
+
+std::ostream& operator<<(std::ostream& out, const Employee& employee)
+{
+    out << "id: " << employee.id << " age: " << employee.age << " wage: " << employee.wage;
+    return out;
+}
+
 int main()
 {
-    Employee joe { 14, 32, 60000.0 };
+    Employee joe { 14, 32, 60000.0 }; // list initialization using braced list (preferred)
+
+    Computer computer {};
+    std::cout << computer << '\n';
+
+    const Employee constEmployee {18, 21, 35000.0};
+
+    Employee e1 { .age{ 30 }, .id{ 19 }, .wage{ 60000.0 } }; // C++20
+    Employee e2 { .age = 30, .id = 19, .wage = 60000.0 }; // C++20
+
+    e1 = { 31, 19, 61000.0 };
+    e2 = {.age = 30, .id = 20, .wage = e2.wage };
+    std::cout << e2 << '\n'; 
+
+    Employee a1 = e2; // copy-initialization
+    Employee a2(e2);  // direct-initialization
+    Employee a3 {e2}; // direct-list-initialization
 
     Employee frank {};
     frank.id = 15;
@@ -31,6 +66,6 @@ int main()
 
     // Today is Joe's birthday
     ++joe.age;
-    
+
     return 0;
 }
