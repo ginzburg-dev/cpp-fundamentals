db4edfa485bb2ccc70800b5e2b49c3139185c239
ginzburg-dev <ginzburgdd@gmail.com>
2025-03-25 12:36:32 +0000

    Modify array_reference_wrapper.cpp

--- arrays/array_reference_wrapper.cpp
+++ arrays/array_reference_wrapper.cpp
@@ -2,6 +2,8 @@
 #include <array>
 #include <functional> // for std::reference_wrapper
 
+// std::reference_wrapper can be used in arrays that contain with references
+
 int main()
 {
     int x { 5 };
@@ -15,6 +17,6 @@ int main()
     std::array<std::reference_wrapper<int>, 1> arr1 { x };
     arr1[0].get() = 5; // modify the object in array element 0
     std::cout << arr1[0] << '\n';
-    
+
     return 0;
 }
