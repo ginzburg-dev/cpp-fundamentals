3b4ed36ee17f81807becb689c963f871a1ab25b1
ginzburg-dev <ginzburgdd@gmail.com>
2025-05-16 15:53:47 +0100

    Add composition.cpp

--- object_relationships/composition.cpp
+++ object_relationships/composition.cpp
@@ -2,10 +2,14 @@
 #include <iostream>
 #include <memory>
 
+// A good rule of thumb is that each class should be built to accomplish a single task. 
+// That task should either be the storage and manipulation of some kind of data (e.g. Point2D, std::string), 
+// OR the coordination of its members (e.g. Creature). Ideally not both.
+
 int main()
 {
-    Creature monster{ "Shaun", {1, 2} };
+    Creature monster{ "Hiss", {1, 2} }; // Creature composed of Poin2D class and std::string
     std::cout << monster << '\n';
-    
+
     return 0;
 }
