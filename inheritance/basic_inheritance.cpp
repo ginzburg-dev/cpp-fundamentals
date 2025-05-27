1db3eb5d4c81ef3fde3acf594819080024a6e2f5
ginzburg-dev <ginzburgdd@gmail.com>
2025-05-27 02:07:52 +0100

    Add basic_inheritance.cpp

---
+++ inheritance/basic_inheritance.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+
+class Person
+{
+public:
+    std::string m_name{};
+    int m_age{};
+
+    Person(std::string_view name="", int age=0)
+        : m_name{name}, m_age{age}
+    {}
+
+    const std::string& getName() const { return m_name; }
+    int getAge() const { return m_age; }
+};
+
+// BaseballPlayer class publicly inheriting Player class
+class BaseballPlayer : public Person
+{
+public:
+    double m_battingAverage{};
+    int m_homeRuns{};
+
+    BaseballPlayer(double battingAverage=0, int homeRuns=0)
+        : m_battingAverage{battingAverage}, m_homeRuns{homeRuns}
+    {}
+};
+
+class Emplayee : public Person
+{
+public:
+    double m_hourlySalary{};
+    long m_employeeID{};
+
+    Emplayee(double hourlySalary=0.0, long employeeID=0)
+        : m_hourlySalary{hourlySalary}, m_employeeID{employeeID}
+    {}
+
+    void print() const
+    {
+        std::cout << m_name << ": " << m_hourlySalary << '\n';
+    }
+};
+
+int main()
+{
+    BaseballPlayer joe{};
+    joe.m_name = "Joe";
+    std::cout << joe.getName() << '\n';
+
+    Emplayee frank{20.25, 12345};
+    frank.m_name = "Frank";
+    frank.print();
+
+    return 0;
+}
