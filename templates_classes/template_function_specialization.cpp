5bd67b579e329a01d1c852f7c7c57df759b75203
ginzburg-dev <ginzburgdd@gmail.com>
2025-06-26 22:50:46 +0100

    template_function_specialization.cpp

---
+++ templates_classes/template_function_specialization.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+template<typename T>
+void print(const T& p)
+{
+    std::cout << p << '\n';
+}
+
+// Specialization by defining a non-template function has the highest precedence
+void print(int p)
+{
+    std::cout << "Max precedence (int) specialization: " << p << '\n';
+}
+
+template<>
+void print<double>(const double& p)
+{
+    std::cout << "Scientific specialization: " << std::scientific << p << '\n';
+}
+
+template<>
+void print<std::string>(const std::string& p) = delete;
+
+int main()
+{
+    print(2); // Prints: Max precedence (int) specialization: 2
+    print(2.7447469); // Prints: Scientific specialization: 2.744747e+00
+    // print(std::string("C-String")); // Error: call to deleted function print
+
+    return 0;
+}
